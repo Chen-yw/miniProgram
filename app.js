c13202aaee73a3e363580a2e0fb9399ba610b2a6
chenyawei <781854554@qq.com>
2021-09-25 14:56:37 +0800

    数据绑定、wx:for、事件、获取用户信息的三种方法、使用全局数据、生命声明周期函数、注册小程序和页面

---
+++ app.js
@@ -0,0 +1,37 @@
+// 注册一个小程序
+App({
+
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: function () {
+    
+  },
+
+  /**
+   * 当小程序页面显示出来，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function (options) {
+    
+  },
+
+  /**
+   * 当小程序从前台进入后台（小程序关闭或者进入后台），会触发 onHide
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError: function (msg) {
+    
+  },
+
+  /* 定义全局变量，在小程序中的任何页面都能共享 */
+  globalData: {
+    name: 'Chen-yw',
+    age:18
+  }
+})
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,79 @@
-// 创建page及对象
-Page({
+ /* 创建page及对象 js文件写逻辑 */
+// getApp()获取add.js中APP()产生的示例对象，保存着APP({})中的数据
+const app = getApp();
+const name = app.globalData.name; // 获取全局变量
+const age = app.globalData.age;
 
+// 注册一个页面
+Page({
+  data: {
+    name: "coderwhy",
+    age: 18,
+    students: [  
+      {id: 110, name: 'kobe', age: 18},
+      {id: 110, name: 'james', age: 18},
+      {id: 110, name: 'why', age: 18},
+    ],
+    counter: 0
+  },
+
+  handleBtnClick() {
+    console.log('按钮发生了点击事件！');
+    // this.data.counter += 1;  // 1.错误做法，不能实时改变页面中显示的数据
+    // 2.this.setData()
+    this.setData({
+      counter: this.data.counter + 1
+    })
+  },
+  handleBtnsub() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
+  },
+
+  // 获取用户信息
+  handleGetUserInfo(event) {
+    console.log(event);
+  },
+
+   /*-------- 1，监听页面的生命周期 --------- */
+  // 页面初次渲染完成后回调
+  onReady() {
+    console.log('onReady');
+  },
+
+  // 页面被加载出来
+  onLoad() {
+    console.log('onLoad');
+    // wx.request({
+    //   // 需要配置后发送网络才能发送网络请求
+    //   url: 'http://123.207.32.32:8000/recommend',
+    //   success(res) {
+    //     console.log(res);
+    //   }
+    // })
+  },
+
+  // 页面被显示出来时
+  onShow() {
+    console.log('onShow');
+  },
+
+  // 页面被隐藏
+  onHide() {
+    console.log('onHide');
+  },
+
+  // 跳转到别的页面
+  onUnload() {
+    console.log('onUnload');
+  },
+
+
+  // ------------ 4，其他事件---------
+  // 监听页面滚动
+  onPageScroll(obj) {},
+
+  // 监听页面滚动到底部
+  onReachBottom() {}
 })
